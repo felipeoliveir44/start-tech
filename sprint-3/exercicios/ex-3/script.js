ba54816830795c5c0cf1876cc3a0fdf73196617d
felipeoliveir44 <felipeoliveiragg@outlook.com>
2023-12-14 17:06:14 -0300

    Exercicios

--- sprint-3/exercicios/ex-3/script.js
+++ sprint-3/exercicios/ex-3/script.js
@@ -1,16 +1,33 @@
 let input = document.querySelector("input");
 let button = document.querySelector("button");
+let buttonListar = document.getElementById("botaoMostrarLista");
 let div = document.querySelector("div");
+div.style.display = "none";
 
 let codigoCliente = 0;
 
 function pulaLinha() {
-    document.createElement("br");
+    document.write("<br>");
 }
 
 function divisor() {
     div.appendChild(document.createElement("hr"));
 }
+
+button.addEventListener("click", function() {
+    adicionarCategoria();
+});
+
+buttonListar.addEventListener("click", function() {
+    if (buttonListar.innerHTML === "Exibir lista") {
+        div.style.display = "block";
+        buttonListar.innerHTML = "Esconder lista";
+
+    } else {
+        div.style.display = "none";
+        buttonListar.innerHTML = "Exibir lista";
+    }
+})
 
 function adicionarCategoria() {
     // Cria um elemento de paragrafo
@@ -21,10 +38,10 @@ function adicionarCategoria() {
     let texto = `<p> Código: ${codigoCliente} <br> Nome: ${input.value}`
     // criarP recebe a variavel de texto
     criarP.innerHTML = texto;
-
+    alert("Categoria cadastrada com sucesso");
     // Irá adicionar um elemento filho a div (Pai)
     div.appendChild(criarP);
     divisor();
 }
 
-button.onclick = adicionarCategoria;
+
