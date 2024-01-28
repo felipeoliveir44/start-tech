2f5eb7a192612108c9ca25636277ddc665e608a1
felipeoliveir44 <felipeoliveiragg@outlook.com>
2024-01-28 16:11:50 -0300

    Sprint 6 - Concluida

---
+++ sprint-6/exercicios/ex-3/ex3.java
@@ -0,0 +1,18 @@
+public class ex3 {
+    public static void main(String[] args) {
+        String divider = "____________________________";
+        String nomeLivro = "Livro";
+        double precoLivro = 49.95;
+
+        String nomeCelular = "Celular";
+        double precoCelular = 1470.76;
+
+        System.out.println(">>>>>>> Listagem de clientes");
+        System.out.println("Nome: " + nomeLivro);
+        System.out.println("Preco: R$ " + precoLivro);
+        System.out.println(divider);
+
+        System.out.println("Nome: " + nomeCelular);
+        System.out.println("Preco: R$ " + precoCelular);
+    }
+}
---
+++ sprint-6/exercicios/ex-4/ex4.java
@@ -0,0 +1,20 @@
+public class ex4 {
+    public static void main(String[] args) {
+
+
+            String divider = "____________________________";
+            String nome1 = "Luiz";
+            int dataNascimento1 = 2003;
+
+            String nome2 = "Fulano";
+            int dataNascimento2 = 1980;
+
+            System.out.println(">>>>>>> Listagem de clientes");
+            System.out.println(divider);
+            System.out.println("Nome: " + nome1);
+            System.out.println("Data de nascimento: " + dataNascimento1);
+            System.out.println(divider);
+            System.out.println("Nome: " + nome2);
+            System.out.println("Data de nascimento: " + dataNascimento2);
+        }
+}
